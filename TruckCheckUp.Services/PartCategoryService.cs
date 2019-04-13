88b8263ec3689f7a5fd6decc012e1804cc2c4950
victor perez <victor.perezmor@gmail.com>
2019-04-13 15:04:46 -0400

    Part Category Admin Page Created
    Used Razor for this case for speed purposes

---
+++ TruckCheckUp.Services/PartCategoryService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruckCheckUp.Core.Contracts.DataAccess;
+using TruckCheckUp.Core.Models;
+using TruckCheckUp.Core.ViewModels.CategoryUI;
+
+namespace TruckCheckUp.Services
+{
+    public class PartCategoryService
+    {
+        private IRepository<PartCategory> _partCategoryContext;
+
+        public PartCategoryService(IRepository<PartCategory> partCategoryContext)
+        {
+            _partCategoryContext = partCategoryContext;
+        }
+
+        // GET: Driver
+        public List<PartCategory> RetrieveAllCategories()
+        {
+            //Get a list of drivers
+            var partCategoriesRetrieved = _partCategoryContext.Collection().ToList();
+
+            return partCategoriesRetrieved;
+        }
+
+        public CategoryInsertViewModel CreateNewPartCategoryObject()
+        {
+            var partCategory = new CategoryInsertViewModel();
+
+            return partCategory;
+        }
+
+        public void PostNewPartCategoryToDB(CategoryInsertViewModel partCategory)
+        {
+            var partCategoryToInsert = new PartCategory();
+            partCategoryToInsert.CategoryPart = partCategory.CategoryPart;
+            _partCategoryContext.Insert(partCategoryToInsert);
+            _partCategoryContext.Commit();
+        }
+
+        public CategoryUpdateViewModel RetrievePartCategoryDataToUpdate(string partCategoryId)
+        {
+            //Look for driver and return her data
+            var partCategoryToUpdate = (
+                    from partCategoryStoredInDB in _partCategoryContext.Collection()
+                    where partCategoryStoredInDB.Id == partCategoryId
+                    select new CategoryUpdateViewModel()
+                    {
+                        Id = partCategoryStoredInDB.Id,
+                        CategoryPart = partCategoryStoredInDB.CategoryPart
+                    }).FirstOrDefault();
+
+            if (partCategoryToUpdate != null)
+            {
+                return partCategoryToUpdate;
+            }
+            else
+            {
+                return new CategoryUpdateViewModel();
+            }
+        }
+
+        public void UpdatePartCategoryData(CategoryUpdateViewModel partCategory, string partCategoryId)
+        {
+            var partCategoryToUpdate = _partCategoryContext.Find(partCategoryId);
+            if (partCategoryToUpdate != null)
+            {
+                partCategoryToUpdate.CategoryPart = partCategory.CategoryPart;
+                _partCategoryContext.Commit();
+            }
+        }
+
+        public PartCategory RetrievePartCategoryToDelete(string partCategoryId)
+        {
+            var partCategoryToDelete = _partCategoryContext.Find(partCategoryId);
+            if (partCategoryToDelete != null)
+            {
+                return partCategoryToDelete;
+            }
+            else
+            {
+                return new PartCategory();
+            }
+        }
+
+        public void DeletePartCategory(string partCategoryId)
+        {
+            var partCategoryToDelete = _partCategoryContext.Find(partCategoryId);
+            if (partCategoryToDelete != null)
+            {
+                _partCategoryContext.Delete(partCategoryToDelete);
+                _partCategoryContext.Commit();
+            }
+        }
+    }
+}
---
+++ TruckCheckUp.WebUI/Controllers/PartCategoryManagementController.cs
@@ -0,0 +1,86 @@
+﻿using System.Web.Mvc;
+using TruckCheckUp.Core.ViewModels.CategoryUI;
+using TruckCheckUp.Services;
+
+namespace TruckCheckUp.WebUI.Controllers
+{
+    public class PartCategoryManagementController : Controller
+    {
+        // GET: PartCategoryManagement
+        private PartCategoryService _partCategoryService;
+
+        public PartCategoryManagementController(PartCategoryService partCategoryService)
+        {
+            this._partCategoryService = partCategoryService;
+        }
+
+        // GET: List all Part Categories
+        public ActionResult Index()
+        {
+            //Get a list of part categories
+            var partCategories = _partCategoryService.RetrieveAllCategories();
+
+            return View(partCategories);
+        }
+
+        public ActionResult Create()
+        {
+            var partCategory = _partCategoryService.CreateNewPartCategoryObject();
+
+            return View(partCategory);
+        }
+
+        [HttpPost]
+        public ActionResult Create(CategoryInsertViewModel partCategory)
+        {
+            //if errors found return to view, otherwise insert into DB
+            if (!ModelState.IsValid)
+            {
+                return View(partCategory);
+            }
+            else
+            {
+                _partCategoryService.PostNewPartCategoryToDB(partCategory);
+                //Once part category inserted then return to display list of categories
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            //Look for part category in DB
+            var partCategoryToUpdate = _partCategoryService.RetrievePartCategoryDataToUpdate(Id);
+
+            return View(partCategoryToUpdate);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CategoryUpdateViewModel partCategory, string Id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(partCategory);
+            }
+            else
+            {
+                _partCategoryService.UpdatePartCategoryData(partCategory, Id);
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            var partCategoryToDelete = _partCategoryService.RetrievePartCategoryToDelete(Id);
+
+            return View(partCategoryToDelete);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            _partCategoryService.DeletePartCategory(Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
